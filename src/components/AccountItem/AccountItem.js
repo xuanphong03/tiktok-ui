16fd277b69c3e682270c4e88c5c86d8ae0cd6885
Michael Nguyen <ngxphong03@gmail.com>
2023-06-12 23:14:14 +0700

    Giải quyết vấn đề nhiều file index.js

---
+++ src/components/AccountItem/AccountItem.js
@@ -0,0 +1,34 @@
+import classNames from "classnames/bind";
+import styles from './AccountItem.module.scss';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import Image from "../Image";
+import { Link } from "react-router-dom";
+import PropTypes from 'prop-types';
+
+const cx = classNames.bind(styles);
+
+function AccountItem({ data }) {
+    return (
+        <Link to={`@${data.nickname}`} className={cx('wrapper')}>
+            <Image
+                className={cx('avatar')}
+                alt={data.full_name}
+                src={data.avatar}
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>{data.nickname}</span>
+                    {data.tick && <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />}
+                </h4>
+                <span className={cx('username')}>{data.full_name}</span>
+            </div>
+        </Link>
+    );
+}
+
+AccountItem.propTypes = {
+    data: PropTypes.object.isRequired,
+}
+
+export default AccountItem;
--- src/components/GlobalStyles/index.js
+++ src/components/GlobalStyles/index.js
@@ -1,13 +1,1 @@
-import './GlobalStyles.scss'
-import PropTypes from 'prop-types';
-import React from 'react';
-
-function GlobalStyles({ children }) {
-    return React.Children.only(children)
-}
-
-GlobalStyles.propTypes = {
-    children: PropTypes.node.isRequired,
-}
-
-export default GlobalStyles 
+export { default } from './GlobalStyles'
